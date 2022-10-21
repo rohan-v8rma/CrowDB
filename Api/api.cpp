7f24ce59da3cd01ab664851488fca5224681ed3a
Mayhul-Jindal <mayhuljindal@gmail.com>
2022-10-22 00:01:54 +0530

    feat: working api.cpp

--- Api/api.cpp
+++ Api/api.cpp
@@ -1,6 +1,15 @@
-#include <iostream>
-using namespace std;
+#include "crow.h"
+//#include "crow_all.h"
 
-int main(){
-  cout << "Mayhul-jindal" << endl;
+int main()
+{
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "maaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
 }
